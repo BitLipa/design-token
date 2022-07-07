9d87b94d258b70793eb5d7467f304a2ed1102e80
Dario Aschero <dario.aschero@gmail.com>
2022-07-07 12:55:08 +0200

    token split reference

--- sets-trasformer.js
+++ sets-trasformer.js
@@ -3,35 +3,26 @@ const { exec } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 const componentsPath='source/components'
-//joining path of directory 
-const directoryComponentsPath = path.join(__dirname, componentsPath);
-//passsing directoryPath and callback function
-fs.readdir(directoryComponentsPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        exec(`token-transformer ${componentsPath}/${file} tokens/components/${file} --preserveRawValue=true --resolveReferences=false`)
-        console.log(file); 
-    });
-});
 
-const themesPath='source/themes'
-//joining path of directory 
-const directoryThemesPath = path.join(__dirname, themesPath);
-//passsing directoryPath and callback function
-fs.readdir(directoryThemesPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        exec(`token-transformer ${themesPath}/${file} tokens/themes/${file} `)
-        console.log(file); 
-    });
+const sets=[]
+
+fs.readFile("figma-tokens.json", "utf-8", (err, data) => {
+  if (err) {
+    throw err;
+  }
+
+  const tokensObj = JSON.parse(data.toString());
+
+  Object.entries(tokensObj).forEach(([setName, setValues]) => sets.push(setName));
+
+  const allSets = sets.join(',')
+  sets.map(set =>{
+    const isComponent = set.includes('component')
+    const exclude = sets.filter(s => s!==set).join(',')
+    const path=isComponent?'components':'themes'
+    const options=isComponent?'--preserveRawValue=true --resolveReferences=false':''
+    const cmd= `token-transformer figma-tokens.json tokens/${path}/${set}.json ${allSets} ${exclude} ${options}`
+    console.log(`Creating tokens/${path}/${set}.json`)
+    exec(cmd)
+  })
 });
